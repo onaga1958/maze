565a9db92f11e7588f13fb4411e60ba14a1f89fb
onaga1958 <alex--zabelin@mail.ru>
2016-10-10 19:49:51 +0300

    Rewrite squares method arrive into more comprehansive event

--- objects/bullet.py
+++ objects/bullet.py
@@ -25,32 +25,4 @@ class Bullet(Object):
                     game.log("Вы промазали")
                     return True
 
-@register_object("скакалка")
-class Jumping_Rope(Object):
-    "прыгать - проверить: земля ли под Вами?"
-    @staticmethod
-    def action(game, player, action):
-        if action == "прыгать":
-            game.log("Вы попрыгали. Кажется, под Вами и правда земля")
-            return False 
-
-@register_object("дубина")
-class Club(Object):
-    "в/н/л/п - ударить в заданую сторону"
-    @staticmethod
-    def action(game, player, action):
-        if action in DIRECTIONS:
-            hit = False 
-            direction = DIRECTIONS[action]
-            for other in game.players:
-                if other.field == player.field and player.name != other.name and (other.position == player.position
-                        or other.position == (player.position[0] + direction[0], player.position[1] + direction[1])):
-                    game.log("Вы попали игроку {} прямо по голове".format(other.name))
-                    hit = True
-                    if game.fields[other.field].can_move(other.position, direction):
-                        other.position = (other.position[0] + direction[0], other.position[1] + direction[1])
-                        game.fields[other.field].squares[other.position[0]][other.position[1]].arrive(game, other) 
-            if not hit:
-                game.log("Дубина со свистом рассекла воздух")
-            return False
-                        
+                       
